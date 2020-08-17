fadfebcfe405a0ff19df0d027fae1455dca0b08b
Hedgy134117 <hedgy134117@gmail.com>
2020-08-17 16:58:42 -0400

    authentication with registration

---
+++ nationsOnline/authentication/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.CreateUser.as_view(), name='register-user'),
+]
--- nationsOnline/authentication/views.py
+++ nationsOnline/authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
-# Create your views here.
+class CreateUser(generics.CreateAPIView):
+    model = get_user_model
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer
