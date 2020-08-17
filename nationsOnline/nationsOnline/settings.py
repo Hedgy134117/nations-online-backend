1dd073c88b354d6b5b88cc4816d4e589f4a20f77
Hedgy134117 <hedgy134117@gmail.com>
2020-08-17 12:55:39 -0400

    Remove secret key

--- nationsOnline/nationsOnline/settings.py
+++ nationsOnline/nationsOnline/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ctbfv7nmowfzpl2n&l%fl@a*p&i+*sw6h(rbt-rz2%-+l$c3=q'
+with open('./nationsOnline/secrets.txt') as f:
+    SECRET_KEY = f.readline().rstrip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
