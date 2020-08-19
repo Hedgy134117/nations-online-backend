68c0c98a07ffb8318f5c36143d791989d0224a21
Hedgy134117 <hedgy134117@gmail.com>
2020-08-19 16:11:53 -0400

    add __str__ method to biome and region models, fix procfile

--- Procfile
+++ Procfile
@@ -1,7 +1,4 @@
 release: python nationsOnline/manage.py makemigrations --no-input
 release: python nationsOnline/manage.py migrate --no-input
-release: cd nationsOnline
-release: python manage.py test --no-input
-release: cd ../
 
 web: gunicorn --chdir nationsOnline nationsOnline.wsgi
--- nationsOnline/nations/models.py
+++ nationsOnline/nations/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 class Biome(models.Model):
     name = models.CharField(max_length=254)
+    
+    def __str__(self):
+        return self.name
 
 class Region(models.Model):
     biome = models.ForeignKey('nations.Biome', models.CASCADE)
     name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
